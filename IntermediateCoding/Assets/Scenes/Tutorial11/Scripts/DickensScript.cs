cd409a3cc90d4904320a7f2f0219da80569c551d
AndreySylka <andreisylka@gmail.com>
2020-07-22 04:22:41 +0300

    updated tutorial11 code and prefab

---
+++ IntermediateCoding/Assets/Scenes/Tutorial11/Scripts/DickensScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DickensScript : MonoBehaviour
+{
+    public GameObject dickPrefab;
+
+    private int childCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 randomTransform = new Vector3(Random.Range(-10 , +10), transform.position.y , Random.Range(-10 , 10));
+            var _instantiate = Instantiate(dickPrefab ,randomTransform , transform.rotation);
+            _instantiate.transform.parent = transform;
+            childCount = transform.childCount;
+            for (int i = childCount-1; i < childCount; i++)
+            {
+                var child = transform.GetChild(i);
+                child.name = dickPrefab.name + "_"+ i;
+            }
+            print("one more Dick in da base");
+        }
+    }
+}
